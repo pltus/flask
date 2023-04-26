5fec64dc3c57e7e6d9479ca4402acfd704170840
pltus <46567881+pltus@users.noreply.github.com>
2023-04-26 15:55:54 -0700

    test

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
-@app.route("/")
+color_size = {'r': 255, 'g': 255, 'b': 255, 'area': 10}
+
+@app.route('/')
 def index():
-    return render_template('./index.html')
+    return render_template('index.html')
+
+@app.route('/control')
+def control():
+    return render_template('control.html')
+
+@app.route('/pattern')
+def pattern():
+    return render_template('pattern.html', color_size=color_size)
+
+@socketio.on('update_color_size')
+def handle_update_color_size(data):
+    global color_size
+    color_size = data
+    emit('color_size_updated', data, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
